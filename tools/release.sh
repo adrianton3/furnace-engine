2e834a1cd1f32917aa1c9508547f3aed7c4206c7
Adrian Toncean <adrianton3@gmail.com>
2016-02-13 11:48:46 +0100

    Update the release script

--- tools/release.sh
+++ tools/release.sh
@@ -4,6 +4,8 @@ mkdir -p tmp
 cp out/furnace.js tmp/furnace.js
 cp src/js/sampleSpec_1.js tmp/sampleSpec_1.js
 cp src/js/sampleSpec_2.js tmp/sampleSpec_2.js
+cp src/js/Util.js tmp/Util.js
+cp src/js/parser.js tmp/parser.js
 rm out/furnace.js
 
 git checkout gh-pages
@@ -11,6 +13,8 @@ git checkout gh-pages
 cp tmp/furnace.js out/furnace.js
 cp tmp/sampleSpec_1.js src/js/sampleSpec_1.js
 cp tmp/sampleSpec_2.js src/js/sampleSpec_2.js
+cp tmp/Util.js src/js/Util.js
+cp tmp/parser.js src/js/parser.js
 rm -rf tmp
 
 git checkout master -- src/index.html
@@ -18,11 +22,9 @@ git checkout master -- src/editor.html
 git checkout master -- src/conv.html
 git checkout master -- src/parser.html
 
-git checkout master -- src/style/style.css
+git checkout master -- src/style
 
-git checkout master -- src/js/Util.js
 git checkout master -- src/js/conv.js
-git checkout master -- src/js/parser.js
 
 git add -u
 
